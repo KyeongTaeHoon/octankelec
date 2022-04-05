204e70f44604d84e40471e68bf2fee538a312250
Kyeong <taehooon@3c0630355cc2.ant.amazon.com>
2022-04-06 00:46:55 +0900

    TT

--- js/main.js
+++ js/main.js
@@ -1,45 +1,32 @@
 /*conatiner*/
-
 const container = document.querySelector(".container");
-
 /*popup*/
-
 const popup = document.querySelector(".popup");
-
 /*closeBtn*/
-
 const closeBtn = document.querySelector("#closebtn");
 
 /*팝업 닫기 함수*/
-
 const closePopup = () => {
-
 console.log("test2")
 
 /*hide 클래스 추가로 숨김*/
-
 popup.classList.add("hide");
-
   //location.reload();
 
 };
-
 /*popup 열기*/
-
 const openPopup = async (userid, itemid, eventtype) => {
 const result = await fetch(
-        "https://random-data-api.com/api/nation/random_nation?size=30"
+        ""
       );
+
 popup.classList.remove("hide");
-
 const popBody = document.querySelector(".popup-body");
-
-popBody.innerHTML = `<p>${capital}</p><p>${language}</p><p>${nationality}</p>`;
+popBody.innerHTML = `<p>${userid}</p><p>${itemid}</p><p>${eventtype}</p>`;
 
 };
 
 /*팝업 닫기 버튼 클릭 시 클로즈 함수 호출*/
-
 closeBtn.addEventListener("click", (event) => {
   if (event.target === event.currentTarget) {
     closePopup();
@@ -64,6 +51,19 @@ openPopup(userid, itemid, eventtype);
 
 /*popup 열기*/
 
+});
+const view2 = document.querySelector("#view2");
+
+view2.addEventListener('click', (event) => {
+  const id = event.target.getAttribute("class");
+
+  const userid = event?.target?.dataset?.userid;
+console.log(userid)
+  const itemid = event?.target?.dataset?.itemid;
+
+const eventtype = event?.target?.dataset?.eventtype;
+
+openPopup(userid, itemid, eventtype);
 });
 /*page 이동(노드서버로 호출)*/
 const movePage = (userid, itemid, eventtype) => {
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+var AWS = require ('aws-sdk');
+AWS.config.update({region:'us-east-1'});
 
 app.use('/js', express.static(__dirname + "/js"));
 
@@ -14,8 +16,22 @@ app.get('/oct', function(요청, 응답){
 });
 
 app.get('/', function(요청, 응답){
+    
+    var personalizeruntime = new AWS.PersonalizeRuntime();
+    var params = {
+      campaignArn: 'arn:aws:personalize:us-east-1:593182458133:campaign/poc-campaign',
+      numResults: '9',
+      userId: '100'
+    };
+    personalizeruntime.getRecommendations(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+    }); 
+
+    
     응답.sendFile(__dirname + '/index.html');
 
+    
 });
 
 app.use('/css', express.static(__dirname + "/css"));
@@ -23,8 +39,34 @@ app.use('/css', express.static(__dirname + "/css"));
 app.get("/user-inter/:userid/:itemid/:eventtype", (req, res) => {
     console.log("test");
     res.render("detail.html", {
-      userid: "김홍구",
-      itemid: "옥탕크 4월 프로모션",
-      eventtype: "관심",
+      userid: 100,
+      itemid: 3780,
+      eventtype: "watch",
     });
   });
+
+ app.post('/', function(요청, 응답){
+    var params = {
+        eventList: [ /* required */
+          {
+            eventType: 'watch', /* required */
+            sentAt: new Date, /* required */
+            impression: [
+              'STRING_VALUE',
+              /* more items */
+            ],
+            itemId: '3253',
+          },
+          /* more items */
+        ],
+        sessionId: 'STRING_VALUE', /* required */
+        trackingId: '1951d070-420b-477d-a02d-82e14e4c9f96', /* required */
+        userId: '100'
+      };
+      personalizeevents.putEvents(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+      });
+
+ });
+
