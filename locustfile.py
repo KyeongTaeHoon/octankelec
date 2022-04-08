04123dd0b922f573735f12b63e2f6cf8c86dfb47
Kyeong <taehooon@3c0630355cc2.ant.amazon.com>
2022-04-08 16:38:55 +0900

    d

--- locustfile.py
+++ locustfile.py
@@ -1,16 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
-	wait_time = between(5, 9)
+        wait_time = between(5, 9)
 
-	@task(3)
-	def my_task1(self):
-		self.client.get("/index.html")
+        @task(3)
+        def my_task1(self):
+                self.client.get("/index.html")
 
-	@task(5)
-	def my_task2(self):
-		self.client.get("/OctankElectronics_files/images(6)")
+        @task(5)
+        def my_task2(self):
+                self.client.get("/OctankElectronics_files/images(6)")
 
-	@task(2)
-	def my_task3(self):
-		self.client.get("/OctankElectronics_files/5e6131e4afff65c9f59d0fd9_5cdd75f59ec2a00550f8b69a_Variants.png")
+        @task(2)
+        def my_task3(self):
+                self.client.get("/OctankElectronics_files/images(5)")
